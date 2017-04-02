e629e2cf81bcff3143b37a369e332f777fc8166c
Booker C. Bense <bbense@gmail.com>
2017-04-02 11:29:42 -0700

    Update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Ehelper.Mixfile do
 
   def project do
     [app: :ehelper,
-     version: "0.1.0",
+     version: "0.4.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
